0b7e2b703d09b08c9619a1c90f52db9c3c22d722
kaspernj <kaspernj@gmail.com>
2019-08-11 11:29:56 +0000

    Make it possible to have multiple counter caches for the same relationship

--- lib/awesome_counter_cache/class_methods.rb
+++ lib/awesome_counter_cache/class_methods.rb
@@ -1,7 +1,11 @@
 module AwesomeCounterCache::ClassMethods
   def awesome_counter_cache_for(relation_name, args = {})
+    @awesome_counter_cache_for ||= 0
+    @awesome_counter_cache_for += 1
+
     args[:column_name] ||= "#{model_name.plural}_count"
     args[:delta_magnitude] ||= proc { 1 }
+    args[:id] = @awesome_counter_cache_for
 
     after_initialize do |model|
       awesome_counter_cache_after_initialize(args, model, relation_name)
