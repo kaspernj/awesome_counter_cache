6d65c3b72d4bf474d365d4acf4b5dd103d9caa03
kaspernj <kaspernj@gmail.com>
2019-08-11 10:54:30 +0000

    Worked a lot more on the feature

--- lib/awesome_counter_cache/class_methods.rb
+++ lib/awesome_counter_cache/class_methods.rb
@@ -4,34 +4,19 @@ module AwesomeCounterCache::ClassMethods
     args[:delta_magnitude] ||= proc { 1 }
 
     after_initialize do |model|
-      @awesome_counter_cache_data ||= {}
-      @awesome_counter_cache_data[relation_name] ||= {}
-
-      primary_key_column = self.class.primary_key
-
-      if read_attribute(primary_key_column).nil? # `new_record?` always returns false so check if primary key is nil instead - kaspernj
-        @awesome_counter_cache_data.fetch(relation_name)[:delta_original] ||= 0
-      else
-        @awesome_counter_cache_data.fetch(relation_name)[:delta_original] ||= args.fetch(:delta_magnitude).call(model)
-      end
+      awesome_counter_cache_after_initialize(args, model, relation_name)
     end
 
     after_commit on: :create do |model|
-      @awesome_counter_cache_data.fetch(relation_name)[:delta_current] = args.fetch(:delta_magnitude).call(model)
-      model.create_awesome_counter_cache_for(relation_name, @awesome_counter_cache_data.fetch(relation_name), args)
-      @awesome_counter_cache_data[relation_name][:delta_original] = @awesome_counter_cache_data[relation_name].delete(:delta_current)
+      awesome_counter_cache_after_commit_on_create(args, model, relation_name)
     end
 
     after_commit on: :destroy do |model|
-      @awesome_counter_cache_data.fetch(relation_name)[:delta_current] = args.fetch(:delta_magnitude).call(model)
-      model.destroy_awesome_counter_cache_for(relation_name, @awesome_counter_cache_data.fetch(relation_name), args)
-      @awesome_counter_cache_data[relation_name][:delta_original] = @awesome_counter_cache_data[relation_name].delete(:delta_current)
+      awesome_counter_cache_after_commit_on_destroy(args, model, relation_name)
     end
 
     after_commit on: :update do |model|
-      @awesome_counter_cache_data.fetch(relation_name)[:delta_current] = args.fetch(:delta_magnitude).call(model)
-      model.update_awesome_counter_cache_for(relation_name, @awesome_counter_cache_data.fetch(relation_name), args)
-      @awesome_counter_cache_data[relation_name][:delta_original] = @awesome_counter_cache_data[relation_name].delete(:delta_current)
+      awesome_counter_cache_after_commit_on_update(args, model, relation_name)
     end
   end
 end
--- lib/awesome_counter_cache/instance_methods.rb
+++ lib/awesome_counter_cache/instance_methods.rb
@@ -1,4 +1,35 @@
 module AwesomeCounterCache::InstanceMethods
+  def awesome_counter_cache_after_commit_on_create(args, model, relation_name)
+    @awesome_counter_cache_data.fetch(relation_name)[:delta_current] = args.fetch(:delta_magnitude).call(model)
+    model.create_awesome_counter_cache_for(relation_name, @awesome_counter_cache_data.fetch(relation_name), args)
+    @awesome_counter_cache_data[relation_name][:delta_original] = @awesome_counter_cache_data[relation_name].delete(:delta_current)
+  end
+
+  def awesome_counter_cache_after_commit_on_destroy(args, model, relation_name)
+    @awesome_counter_cache_data.fetch(relation_name)[:delta_current] = args.fetch(:delta_magnitude).call(model)
+    model.destroy_awesome_counter_cache_for(relation_name, @awesome_counter_cache_data.fetch(relation_name), args)
+    @awesome_counter_cache_data[relation_name][:delta_original] = @awesome_counter_cache_data[relation_name].delete(:delta_current)
+  end
+
+  def awesome_counter_cache_after_commit_on_update(args, model, relation_name)
+    @awesome_counter_cache_data.fetch(relation_name)[:delta_current] = args.fetch(:delta_magnitude).call(model)
+    model.update_awesome_counter_cache_for(relation_name, @awesome_counter_cache_data.fetch(relation_name), args)
+    @awesome_counter_cache_data[relation_name][:delta_original] = @awesome_counter_cache_data[relation_name].delete(:delta_current)
+  end
+
+  def awesome_counter_cache_after_initialize(args, model, relation_name)
+    @awesome_counter_cache_data ||= {}
+    @awesome_counter_cache_data[relation_name] ||= {}
+
+    primary_key_column = self.class.primary_key
+
+    if read_attribute(primary_key_column).nil? # `new_record?` always returns false so check if primary key is nil instead - kaspernj
+      @awesome_counter_cache_data.fetch(relation_name)[:delta_original] ||= 0
+    else
+      @awesome_counter_cache_data.fetch(relation_name)[:delta_original] ||= args.fetch(:delta_magnitude).call(model)
+    end
+  end
+
   def create_awesome_counter_cache_for(relation_name, state_data, args)
     relation_model = __send__(relation_name)
     return if relation_model.blank?
